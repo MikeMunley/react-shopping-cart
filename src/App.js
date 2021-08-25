570bfaf4ef8e2afc65c869492828e65a1d91b111
Mike Munley <spex1990@gmail.com>
2021-08-25 16:26:43 -0500

    Added Product Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
+import React from "react";
+import Products from "./components/Products.js";
+import data from "./data.json";
 
-
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    }
+  }
+  render() {
   return (
    <div className="grid container">
      <header>
        <a href="/">React Shopping Cart</a>
      </header>
      <main>
-       Product List
+       <div className="content">
+         <div className="main">
+          <Products products={this.state.products} />
+         </div>
+         <div className="sidebar">
+          Cart Items
+         </div>
+       </div>
      </main>
      <footer>
        All Rights Reserved
      </footer>
    </div>
   );
+}
 }
 
 export default App;
